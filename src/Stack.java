578c1503040d2c1fc9626b18d18466128fdcb3a5
Georgeofgithub <lucasbuhelt@gmail.com>
2022-03-03 08:10:32 +0100

    changes

---
+++ src/Stack.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Stack {
+
+    public static void main(String[] args) {
+        int number = 0;
+        String input = null;
+        Scanner scanner= new Scanner(System.in);
+        int length = scanner.nextInt();
+        int[] array = new int[length];
+
+        for (int i=0; i<=length; i++) {
+            input scanner.nextLine();
+        }
+        
+    }
+    
+    public void Push(int n) {
+
+    }
+
+    public void Pop() {
+
+    }
+}
